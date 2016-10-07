926eb1b1dedbd6903b8fed4a203c89d20ec2cfc7
Ben Boyle <benjamins.boyle@gmail.com>
2016-10-07 18:40:02 +1000

    consistent indenting
    code is easier to read when indented consistently. I've tried to stick to the style in place which looks like 2 spaces for indents, and `{` at the end of a line, and `else` also on its own new line.
    
    you should see nice vertical lines where code starts and finishes: e.g. the beginning and end of the for loop, and each if/else block.

--- js/app.js
+++ js/app.js
@@ -1,28 +1,26 @@
 $(document).ready(function() {
-for (var i = 1; i <= 100; i++) 
-{
-   if (i % 15 == 0) {
-  console.log("FizzBuzz");
-  $("body").append("Fizz Buzz");
-  $("body").append("<br/>");
-}
-else if ( i % 3 == 0) {
-  console.log("Fizz");
-  $("body").append("Fizz");
-  $("body").append("<br/>");
-}
-else if ( i % 5 == 0) {
-  console.log("Buzz");
-  $("body").append("Buzz");
-  $("body").append("<br/>");
-}
-else 
-{
-console.log(i);
-$("body").append(i);
-$("body").append("<br/>");
-}
-}
+  for (var i = 1; i <= 100; i++) {
+    if (i % 15 == 0) {
+      console.log("FizzBuzz");
+      $("body").append("Fizz Buzz");
+      $("body").append("<br/>");
+    }
+    else if ( i % 3 == 0) {
+      console.log("Fizz");
+      $("body").append("Fizz");
+      $("body").append("<br/>");
+    }
+    else if ( i % 5 == 0) {
+      console.log("Buzz");
+      $("body").append("Buzz");
+      $("body").append("<br/>");
+    }
+    else {
+      console.log(i);
+      $("body").append(i);
+      $("body").append("<br/>");
+    }
+  }
 });
 
 
